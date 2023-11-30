5748eb4594c5757a979a6ea4e92260bb212045d7
shahid1105 <shahidhasan1105@gmail.com>
2023-11-30 21:58:16 +0600

    final push

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
